ed8abbfed2990894c14f7a6ade36e19e9b40bbfe
Abdul Dakkak <adakkak@modular.com>
2022-06-04 05:42:11 -0700

    checkpoint

--- src/vector-add/naive.cpp
+++ src/vector-add/naive.cpp
@@ -3,13 +3,15 @@
 #include <benchmark/benchmark.h>
 #include <vector>
 
+using ElemType = float;
+
 static void VectorAdd_Naive(benchmark::State &state) {
   const auto size = 1ULL << static_cast<size_t>(state.range(0));
   for (auto _ : state) {
     state.PauseTiming();
-    std::vector<float> a(size, 1);
-    std::vector<float> b(size, 1);
-    std::vector<float> c(size, 0);
+    std::vector<ElemType> a(size, 1);
+    std::vector<ElemType> b(size, 1);
+    std::vector<ElemType> c(size, 0);
     auto aData = a.data(), bData = b.data(), cData = c.data();
     state.ResumeTiming();
     for (size_t i = 0; i < size; i++) {
@@ -18,7 +20,9 @@ static void VectorAdd_Naive(benchmark::State &state) {
     benchmark::DoNotOptimize(c);
   }
 
-  state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(size));
+  state.SetBytesProcessed(
+      int64_t(state.iterations() * size * sizeof(ElemType)));
+  state.SetItemsProcessed(int64_t(state.iterations() * size));
   state.counters["size"] = double(size);
 }
 BENCHMARK(VectorAdd_Naive)->SMALL_ARGS();
